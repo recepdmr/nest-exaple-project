3adbc5fe4b15853b8934ac50eb65f97d215616ea
recepdmr <recepdemir3438@gmail.com>
2021-11-28 02:05:40 +0300

    Added getProducts method test.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Product } from './domain/entities/product';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -17,6 +18,10 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+
+    it('cannot be null product array"', () => {
+      expect(appController.getProducts()).not.toBeNull();
     });
   });
 });
